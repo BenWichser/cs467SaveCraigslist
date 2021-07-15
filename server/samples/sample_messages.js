d4abee79d9924d6d9557ceeedb10ff13612f6337
Ben Wichser <wichserpdx@gmail.com>
2021-07-15 09:13:38 -0700

    messages send

---
+++ server/samples/sample_messages.js
@@ -0,0 +1,77 @@
+/* Sample Messages for SaveCraigsList */
+
+const messageTemplate = {
+    'id': 'S',
+    'sender_id': 'S',
+    'receiver_id': 'S',
+    'date_sent': 'S',
+    'content': 'S'
+}
+
+
+const sampleMessageA = {
+    id: 'sampleMessageA',
+    sender_id: 'jbutt',
+    receiver_id: 'joebiden46',
+    date_sent: '1626364720344',
+    content: 'Thanks for the glasses, man.  Good luck in office!  Keep cool.'
+}
+
+const sampleMessageB = {
+    id: 'sampleMessageB',
+    sender_id: 'joebiden46',
+    date_sent: '1626364830820',
+    content: "You're welcome.  Back to work."
+}
+
+const sampleMessageC = {
+    id: 'sampleMessageC',
+    sender_id: 'art',
+    receiver_id: 'josephine_darakjy',
+    date_sent: '1626365018553',
+    content: 'It sure is.  However, it is not yet in full mold bloom.  Still yellow.  Are you interested?'
+}
+
+const sampleMessageD = {
+    id: 'sampleMessageD',
+    sender_id: 'josephine_darakjy',
+    receiver_id: 'art',
+    date_sent: '1626364919663',
+    content: 'Is the Danish still available?'
+}
+
+const sampleMessageE = {
+    id: 'sampleMessageE',
+    sender_id: 'art',
+    receiver_id: 'josephing_darakjy',
+    date_sent: '1626365089305',
+    content: "Helllo?  Are you hungry for Art's art?"
+}
+
+const sampleMessageF = {
+    id: 'sampleMessageF',
+    sender_id: 'josephine_darakjy',
+    receiver_id: 'art',
+    date_sent: '1626365081000',
+    content: "I'm afraid not.  I just had a salad with blue cheese dressing.  Good luck!"
+}
+
+const sampleMessageG = {
+    id: 'sampleMessageG',
+    sender_id: 'art',
+    receiver_id: 'jbutt',
+    date_sent: '1626365261395',
+    content: 'Hey there.  Wanna play ball this weekend?'
+}
+
+sampleMessages = [
+    sampleMessageA,
+    sampleMessageB,
+    sampleMessageC,
+    sampleMessageD,
+    sampleMessageE,
+    sampleMessageF,
+    sampleMessageG
+];
+
+module.exports = {sampleMessages};
